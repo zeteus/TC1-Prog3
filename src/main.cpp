1ddd718a638a320a9de383f36c5e2663841174b3
Henrique Layber <henriquelayber@gmail.com>
2019-09-25 15:38:47 -0300

    Essas classe maluca n tem como não

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,43 @@
+#include "../include/defines.h" //corzinhas
 #include <iostream>
-#include "../include/defines.h"
-#include "../include/CSV_RW.hpp"
+#include <fstream>
+
+using std::cerr;
+using std::cout;
+using std::string;
+
+template<typename type>
+type readCSV(std::ifstream infile){
+    type data;
+    infile >> data;
+    return data;
+}
+
+
+template <typename type>
+void writeCSV(std::ofstream outfile, type data){
+    outfile << data << ',';
+}
+
+void lineBreakCSV(std::ofstream outfile){
+    outfile << '\n';
+}
 
 int main() {
-    CSV_RW file = CSV_RW("output.csv");
-    file.writeValue(10);
+    string outfileName = "output.csv";
+    string infileName = "input.csv";
+
+
+    std::ofstream outfile;
+    std::ifstream infile;
+    outfile.open(outfileName, std::ios::out);
+    if(!outfile.is_open()){
+        cerr << _BOLDRED << "Erro ao abrir o arquivo \"" << outfileName << "\"." << _RESET << "Verifique se ele existe. Saindo...\n";
+        exit(1);
+    }
+    infile.open(infileName, std::ios::in);
+    if(!infile.is_open()){
+        cerr << _BOLDRED << "Erro ao abrir o arquivo \"" << infileName << "\"." << _RESET << "Verifique se ele existe. Saindo...\n";
+        exit(1);
+    }
 }
