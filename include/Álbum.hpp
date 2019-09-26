f1da2054265d84d0339a3ca501b7fa581519762b
Ezequiel <quielemao@gmail.com>
2019-09-26 10:42:54 -0300

    Álbum modificado

--- include/Álbum.hpp
+++ include/Álbum.hpp
@@ -1,11 +1,41 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
 #include "Artista.hpp"
+
+using namespace std::string;
+using namespace ios::ofstream;
 
 class Album {
 
 private:
     Artista* artist;
 public:
+    int duration;
+    int yearLaunch;
+    int musicAmount;
+    string name;
+    
+    // Constructor
+    Album(Artista* _artist);
     Album();
+    // Destructor
     ~Album();
+
+    // Functions
+    void imprimeNoArquivo(ofstream &_outfile);
+
+    // getters
+    Artista* getArtista();
+    int getDuration();
+    int getYearLaunch();
+    int getMusicAmount();
+    string getName();
+    // setters
+    void setArtista(Artista* _artist);
+    void setDuration(int _duration);
+    void setYearLaunch(int _yearLaunch);
+    void setMusicAmount(int _musicAmount);
+    void setName(string _name);
 };
--- src/Álbum.cpp
+++ src/Álbum.cpp
@@ -1,4 +1,14 @@
+#include <iostream>
+#include <string>
 #include "Álbum.hpp"
+
+Album::Album(Artista* _artist, int _duration, int _yearLaunch, int _musicAmount, string _name) {
+    Album::setName(_name);
+    Album::setArtista(_artist);
+    Album::setDuration(_duration);
+    Album::setYearLaunch(_yearLaunch);
+    Album::setMusicAmount(_musicAmount);
+}
 
 Album::Album() {
 
@@ -6,4 +16,52 @@ Album::Album() {
 
 Album::~Album() {
 
+}
+
+Artist* Album::getArtista() {
+    return *this->artist;
+}
+
+void Album::imprimeNoArquivo(ofstream &_outfile) {
+    _outfile << "DURACAO:" << this->duration << endl;
+    _outfile << "ANO DE LANCAMENTO:" << this->yearLaunch << endl;
+    _outfile << "QTD MUSICAS:" << this->musicAmount << endl;
+    _outfile << "NOME:" << this->name << endl;
+    _outfile << "ARTISTA:" << *this->artist << endl;
+    _outfile << "______________________________________" << endl;
+}
+
+int getDuration() {
+    return this->duration;
+}
+
+int getYearLaunch() {
+    return this->yearLaunch;
+}
+
+int getMusicAmount() {
+    return this->musicAmount;
+}
+
+string getName() {
+    return this->name;
+}
+
+void Album::setArtista(Artista* _artist) {
+    this->artist = _artist;
+
+void setDuration(int _duration) {
+    this->artist = _duration;
+}
+
+void setYearLaunch(int _yearLaunch) {
+    this->artist = _yearLaunch;
+}
+
+void setMusicAmount(int _musicAmount) {
+    this->artist = _musicAmount;
+}
+
+void setName(string _name){
+    this->artist = _name;
 }
